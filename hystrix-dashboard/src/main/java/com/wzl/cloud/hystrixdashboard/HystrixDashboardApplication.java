9aa2462a087d1fff9d84c74cda8b069181e094d3
wzl <431426221@qq.com>
2020-07-15 14:46:20 +0800

    第一次提交

--- hystrix-dashboard/src/main/java/com/wzl/cloud/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/wzl/cloud/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,8 +2,6 @@ package com.wzl.cloud.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
